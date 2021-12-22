2f9fb73e312e95e0b1d83d216eaad2fde65cbfb1
Tomas Sobota <smeky.sobota@gmail.com>
2021-12-22 17:57:40 +0100

    Added Projects page

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="navigation">
         <NuxtLink to="/">Home</NuxtLink>
+        <NuxtLink to="/projects">Projects</NuxtLink>
         <NuxtLink to="/about">About</NuxtLink>
     </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,5 @@
 <template>
     <Page>
         <h1>Index</h1>
-
-        <iframe src="http://localhost:9001/dist" width="1024" height="768"></iframe>
     </Page>
 </template>
