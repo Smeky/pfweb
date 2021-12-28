5e92aa989dff0ab98de5a00d354c4c41cc590aee
Tomas Sobota <smeky.sobota@gmail.com>
2021-12-28 14:39:09 +0100

    Cleaned up background's code

--- scripts/background.js
+++ scripts/background.js
@@ -1,10 +1,35 @@
 
-const MaxParticleDistance = 300
-const MaxPointSpeed = 100
-const ParticleCount = 200
+const MaxConnectionDistance = 120
+const MaxPointSpeed = 30
+const ParticlesPer1000PxSqrd = 0.2
+const DebugEnabled = false
 
-function distance(first, second) {
-    return Math.hypot(second.x - first.x, second.y - first.y)
+const Vec2 = {
+    distance: (a, b) => Math.hypot(b.x - a.x, b.y - a.y)
+}
+
+let timedFn = null
+let totalTime = 0
+let callCount = 0
+
+function timeIt(fn, ...args) {
+    if (timedFn !== fn) {
+        timedFn = fn
+        callCount = 0
+        totalTime = 0
+    }
+
+    const start = performance.now() 
+    fn(...args)
+    const end = performance.now()
+    
+    totalTime += end - start
+    callCount++
+
+    // Estimate 60 calls per second
+    if (callCount > 0 && callCount % 60 === 0) {
+        console.log(`timeIt avg ${totalTime / callCount}`)
+    }
 }
 
 function createParticleTexture(pixi, app) {
@@ -23,7 +48,8 @@ function createParticles(pixi, texture, count, stageWidth, stageHeight) {
         const scale = 0.1 + Math.random() * 0.9
         const particle = new pixi.Sprite(texture)
         
-        particle.id = i + 1
+        particle.index = i
+        particle.connections = []
         particle.alpha = 0.2 + Math.random() * 0.8
         particle.anchor.set(0.5, 0.5)
         particle.scale.x = scale
@@ -59,20 +85,51 @@ function updateParticles(delta, particles, stageWidth, stageHeight) {
     }
 }
 
-function updateConnections(connections, particles, mousePos) {
-    connections.clear()
-    
+function updateConnections(particles) {
+    particles.forEach((particle) => {
+        particle.connections = particles
+            // Start searching from this particle's index (all previous are already checked)
+            .slice(particle.index + 1)
+            // Faster way to filter out all connections that are too distant (rectangle, not a precise radius circle)
+            .filter((other) => {
+                return particle.position.x - other.position.x < MaxConnectionDistance &&
+                       particle.position.y - other.position.y < MaxConnectionDistance
+            })
+            .filter((other) => {
+                return !particle.connections.some((connectee) => connectee === other.index)
+            })
+            .reduce((connections, other) => {
+                if (MaxConnectionDistance >= Vec2.distance(particle.position, other.position)) {
+                    connections.push(other.index)
+                }1
+
+                return connections
+            }, particle.connections)
+    })
+}
+
+function redrawLines(lines, particles) {
+    lines.clear()
+
     for (const particle of particles) {
-        const distToMouse = distance(particle.position, mousePos)
-        
-        if (distToMouse < MaxParticleDistance) {
-            connections.lineStyle(1 - distToMouse / MaxParticleDistance, 0xffffff, 1 - distToMouse / MaxParticleDistance)
-            connections.moveTo(particle.position.x, particle.position.y)
-            connections.lineTo(mousePos.x, mousePos.y)
-        }
+        particle.connections.forEach((connectee, index) => {
+            const otherPos = particles[connectee].position
+            const distance = Vec2.distance(particle.position, otherPos)
+
+            if (distance > MaxConnectionDistance) {
+                particle.connections.splice(index, 1)
+            }
+            else {
+                const ratio = 1 - distance / MaxConnectionDistance
+
+                lines.lineStyle(0.3 + ratio * 0.7, 0xffffff, ratio)
+                lines.moveTo(particle.position.x, particle.position.y)
+                lines.lineTo(otherPos.x, otherPos.y)
+            }
+        })
     }
 
-    connections.endFill()
+    lines.endFill()
 }
 
 async function runAnimation() {
@@ -89,27 +146,38 @@ async function runAnimation() {
         height: stageHeight
     })
 
+    const particleCount = Math.floor((stageWidth * stageHeight) / 1000 * ParticlesPer1000PxSqrd)
     const particleTex = createParticleTexture(pixi, app)
-    const particles = createParticles(pixi, particleTex, ParticleCount, stageWidth, stageHeight)
-    const connections = new pixi.Graphics()
-    const ticker = pixi.Ticker.shared
+    const particles = createParticles(pixi, particleTex, particleCount, stageWidth, stageHeight)
+    const lines = new pixi.Graphics()
+    const ticker = new pixi.Ticker()
 
     app.stage.addChild(...particles)
-    app.stage.addChild(connections)
-
-    let mousePos = { x: 0, y: 0 }
-    app.stage.interactive = true
-    app.stage.on("mousemove", (event) => {
-        mousePos.x = event.data.global.x
-        mousePos.y = event.data.global.y
-    })
+    app.stage.addChild(lines)
 
     ticker.add((time) => {
         const delta = time / 60
 
         updateParticles(delta, particles, stageWidth, stageHeight)
-        updateConnections(connections, particles, mousePos)
+        updateConnections(particles)
+        
+        redrawLines(lines, particles)
     })
+
+    if (DebugEnabled) {
+        const g = app.stage.addChild(new pixi.Graphics())
+
+        ticker.add(() => {
+            const p = particles[0]
+
+            g.clear()
+            g.lineStyle(1, 0xff0000)
+            g.moveTo(p.position.x, p.position.y)
+            g.lineTo(p.position.x + MaxConnectionDistance, p.position.y)
+            g.drawCircle(p.position.x, p.position.y, MaxConnectionDistance)
+            g.endFill()
+        })
+    }
 
     ticker.start()
 }
