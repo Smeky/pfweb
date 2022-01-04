2aad9551f79c582791eb560c47d93ecf295b7d4d
Smeky <smeky.sobota@gmail.com>
2022-01-04 23:48:23 +0100

    Added smooth transition to nav bar

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,28 +1,72 @@
 <template>
-    <div class="navigation">
-        <a href="#root">Home</a>
-        <a href="#projects">Projects</a>
-        <a href="#skills">Skills</a>
+    <div id="navbar">
+        <div class="navigation" v-bind:class="{ '--fixed': isFixed }">
+            <a href="#root">Home</a>
+            <a href="#projects">Projects</a>
+            <a href="#skills">Skills</a>
+        </div>
     </div>
 </template>
 
 <style lang="scss">
-    .navigation {
-        position: fixed;
-        top: 0;
-        right: 0;
+    $navbar-height: 50px;
 
+    #navbar {
+        height: $navbar-height;
+        margin-bottom: 40px;
+    }
+
+    .navigation {
         display: flex;
-        height: 50px;
+        height: $navbar-height;
         flex-direction: row;
         flex-wrap: wrap;
-        justify-content: flex-end;
-        padding: 6px 20px 6px 0;
+        justify-content: center;
+        transition: all 0.7s ease;
+
+        position: absolute;
+        right: 50%;
+        transform: translateX(50%);
+        z-index: 1;
 
         > a {
             padding: 8px 12px;
             font-size: 20px;
             height: 30px;
         }
+
+        &.--fixed {
+            position: fixed;
+            top: 0;
+            right: 40px;
+            transform: translate(0%);
+        }
     }
 </style>
+
+<script>
+    export default {
+        data() {
+            return {
+                isFixed: false
+            }
+        },
+
+        mounted() {
+            window.addEventListener("scroll", this.handleScroll)
+        },
+        
+        destroyed() {
+            window.removeEventListener("scroll", this.handleScroll)
+        },
+
+        methods: {
+            handleScroll() {
+                const navbar = document.getElementById("navbar")
+                const { top } = navbar.getBoundingClientRect()
+
+                this.isFixed = top < 0
+            }
+        }        
+    }
+</script>
--- src/components/Page.vue
+++ src/components/Page.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="page">
-        <Navigation />
         <slot></slot>
     </div>
 </template>
